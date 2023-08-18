faadd098ddbcac41fa09e45ee8655fd236f59dac
Ribarra18 <Renatothefourth@gmail.com>
2023-08-18 09:49:53 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const form = document.querySelector("form");
+const submitButton = document.querySelector("#submit");
+const memeSelection = document.querySelector(".memes");
+const inputs = document.querySelectorAll("input:not(#submit)");
+
+submitButton.addEventListener("click", (event) => {
+  if (form.checkValidity()) {
+    event.preventDefault();
+
+    const inputsArr = [...inputs];
+    const formattedData = formatData(inputsArr);
+
+    clearInputs(inputsArr);
+
+    addMeme(formattedData);
+  }
+});
+function formatData(data) {
+  return data.reduce(
+    (data, input) => ({ ...data, [input.id]: input.value }),
+    {}
+  );
+}
+
+function clearInputs(data) {
+  data.forEach((input) => (input.value = ""));
+}
+
+function addMeme(data) {
+  const div = document.createElement("div");
+  const image = document.createElement("img");
+  const topText = document.createElement("p");
+  const bottomText = document.createElement("p");
+
+  img.src = data.imageUrl;
+  topText.textContent = data.topText;
+  bottomText.textContent = data.bottomText;
+  topText.classList.add("top-text");
+  bottomText.classList.add("bottom-text");
+
+  div.append(image);
+  div.append(topText);
+  div.append(bottomText);
+
+  div.addEventListener("click", () => {
+    div.remove();
+  });
+
+  memeSelection.append(div);
+}
